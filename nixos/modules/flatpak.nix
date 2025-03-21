a258bca5d64c912e9b765f59575e2069ace797d7
ciara r k. <thekckfactor@gmail.com>
2025-03-21 03:26:41 -0500

    Update flatpak.nix

--- nixos/modules/flatpak.nix
+++ nixos/modules/flatpak.nix
@@ -1,3 +1,9 @@
 {
     services.flatpak.enable = true;
+    services.flatpak = {
+       enabled = true;
+       packages = [
+           { flatpakref = "https://sober.vinegarhq.org/sober.flatpakref" };
+       ];
+   }
 }
