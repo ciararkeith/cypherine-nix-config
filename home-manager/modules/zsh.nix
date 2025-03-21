cb8958a57875c0ed9d63cccf4b31714fb5282914
ciara r k. <thekckfactor@gmail.com>
2025-03-21 03:00:40 -0500

    Update zsh.nix

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -1,3 +1,12 @@
 {
-   programs.fish.enable = true
+   programs.fish = {
+       enable = true;
+       interactiveShellInit = ''
+         function starship_transient_prompt_func
+             starship module character
+         end
+         starship init fish | source
+         enable_transience
+        '';
+   }
 }
