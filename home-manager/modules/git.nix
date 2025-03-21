fefa3e01d3683ba4ccca4d9965dd5d4742d13f24
ciara r k. <thekckfactor@gmail.com>
2025-03-21 02:43:06 -0500

    Update git.nix

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,7 +1,7 @@
 {
   programs.git = {
     enable = true;
-    userName = "Andrey0189";
-    userEmail = "varnavsky06@gmail.com";
+    userName = "ciararobin";
+    userEmail = "ciararkeith@yahoo.com";
   };
 }
